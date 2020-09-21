420368658ea376f25e0793fbd16e8925f3192a20
Tanmoy Banik <imtanmoybanik@gmail.com>
2020-09-21 15:05:24 +0600

    apollo graphql nexus

---
+++ pages/api/graphql.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from "apollo-server-micro";
+import { schema } from "schemas/schema";
+
+const apolloServer = new ApolloServer({ schema });
+const handler = apolloServer.createHandler({ path: "/api/graphql" });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
